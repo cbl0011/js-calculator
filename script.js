dca27b4a1cf9467b112b301cba8a14717a64a1c3
Cody Long <codylong88@yahoo.com>
2019-09-19 14:46:15 -0500

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(operator, firstNum, secondNum) {
+    switch(operator) {
+        case '+':
+            return add(firstNum, secondNum);
+        case '-':
+            return subtract(firstNum, secondNum);
+        case '*':
+            return multiply(firstNum, secondNum);
+        case '/':
+            return divide(firstNum, secondNum);
+        default:
+            return 'ERROR';
+    }
+}
